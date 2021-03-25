a1d4551f06bd6e43da7b983b54b5dd5364aea392
Boris Marin <56419970+BorisMarin8004@users.noreply.github.com>
2021-03-25 15:31:19 -0700

    Redo

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-const int NAME_CHAR_LIMIT = 10;
-//TODO Do we need hand limit?
-//const int HAND_LIMIT = 10;
-
-struct Player{
-    char name[NAME_CHAR_LIMIT];
-    int hand[];
-};
-
 int main() {
 
 
