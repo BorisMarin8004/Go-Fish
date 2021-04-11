f0cb0efe07791f72ac1ce4cbb4e38fb48fb61fc0
Boris Marin <56419970+BorisMarin8004@users.noreply.github.com>
2021-04-10 20:46:27 -0700

    Added comments

--- main.c
+++ main.c
@@ -69,7 +69,8 @@ void testHands(){
     }
 }
 
-//'A','2','3','4','5','6','7','8','9','','J','Q','K'};
+
+//Generates array of integers, e.g. deck of cards, there cards are integer from 0 to 12, and there are 4 of each integer.
 void createDeck() {
     int type[13] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
     for(int i = 0; i < 4; i++ ) {
@@ -92,14 +93,14 @@ void shuffleDeck(int shuffleNumber) {
 }
 
 
-
+//swaps two integers by address.
 void swap (int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-
+//Distributes integers, e.g. cards to each player from deck (numberOfPlayers - how many players there are)
 void dealCards (int numberOfPlayers, int cardPerHand) {
     for(int i = 0; i < cardPerHand; i++){
         for(int j = 0; j < numberOfPlayers; j++){
@@ -109,28 +110,38 @@ void dealCards (int numberOfPlayers, int cardPerHand) {
     }
 }
 
+//Prints options that player can choose. Like choose a player to take a card from.
 void printOptions(int player){
     printf("*Player %d hand\n",player+1);
+    int hasCardInHand = 0;
     for (int j = 0; j < 13; j++){
         if(hands[player][j] != 0 && hands[player][j] != pairLimit) { // change hard coded 4
             printf("You have %d, %d's\n", hands[player][j], j);
+            hasCardInHand = 1;
         }
-
+    }
+    if (!hasCardInHand){
+        printf("You can only fish, your hand is empty.\n");
     }
     printf("Which Player would you like to ask for which card in your hand? (Player Number, Card Number): \n");
 }
 
+//Calls draw() to take card from deck and adds it to player's hand
 int goFish (int player, int expCard){
     int card = draw(deck, deckTop);
     hands[player][card]++;
     return expCard == card;
 }
 
+//Decrease cards from one hand to increases to current player
 void moveCards(int srcPlayer, int targetPlayer, int card){
-    hands[srcPlayer][card]--;
-    hands[targetPlayer][card]++;
+    while(cardInHand(srcPlayer, card)) {
+        hands[srcPlayer][card]--;
+        hands[targetPlayer][card]++;
+    }
 }
 
+//Checks if targetPlayer has desired card
 int cardInHand(int targetPlayer, int card){
     if(hands[targetPlayer][card] > 0){
         return 1;
@@ -139,6 +150,8 @@ int cardInHand(int targetPlayer, int card){
     }
 }
 
+//Handles the turn in Go-Fish game. It calls printOptions(), takes player input, checks is card player inputted is in the opponents hand,
+//if so calls moveCards(), otherwise calls goFish(). If player fished successfully makes recursive call to turn().
 void turn(int targetPlayer){
     printOptions(targetPlayer);
     int playerToAsk;
@@ -157,6 +170,7 @@ void turn(int targetPlayer){
     }
 }
 
+//Increments Deck top and returns card
 int draw () {
     int card = deck[deckTop];
     deckTop = deckTop + 1;
