74d265a522456c0ec9bb6268cd389f638816bc99
keyoni <56132773+keyoni@users.noreply.github.com>
2021-04-11 17:55:01 -0700

    combined update

--- main.c
+++ main.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-<<<<<<< Updated upstream
-
-//for later https://stackoverflow.com/questions/54962681/2d-array-c-to-mips
-
-int* createDeck(); 
-int * shuffleDeck(int* deck, int shuffleNumber);
-void swap (int *a, int*b);
-void dealCards (int hands[4][13], int numberOfPlayer, int cardPerHand, int* deck, int *top);
-void printOptions( int hands[4][13], int player);
-int draw (int*deck, int* deckTop);
-
-int main(void) {
-    int *deck;
-=======
 #include <stdlib.h>
 //for later https://stackoverflow.com/questions/54962681/2d-array-c-to-mips
 const int DECK_SIZE = 52;
@@ -37,7 +23,7 @@ void printOptions(int player);
 int draw();
 
 int isFinished();
-int endGame();
+void endGame();
 void printScores();
 
 
@@ -78,34 +64,20 @@ int main(void) {
 }
 
 void startGame() {
->>>>>>> Stashed changes
     int shuffleNumber;
-    int deckTop = 0;
+
     int numberOfPlayers;
     int players[4];
-    int playerHands[4][13] = {{0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0}};
     int boolFourPair = 0;
-<<<<<<< Updated upstream
-    printf("Enter an integer between 50 - 1000: ");
-=======
     int turnOrder = 0;
     printf("Enter an integer between 50 - 1000(seed): ");
->>>>>>> Stashed changes
     scanf("%d", &shuffleNumber);
-    deck = createDeck();
-    deck = shuffleDeck(deck,shuffleNumber);
+    createDeck();
+    shuffleDeck(shuffleNumber);
 
-    printf("Enter an integer between 2-4: ");
+    printf("Enter number of players between 2-4: ");
     scanf("%d", &numberOfPlayers);
 
-<<<<<<< Updated upstream
-    dealCards(playerHands, numberOfPlayers, 5,  deck, &deckTop);
-
-    //print Options Test
-    printOptions(playerHands, 2);
-
-    return 0;
-=======
     dealCards(numberOfPlayers, 5);
     if(!isFinished()) {
     while(turnOrder != numberOfPlayers){
@@ -121,87 +93,121 @@ void startGame() {
     endGame();
 
     //printf("ENDGAME\n");
->>>>>>> Stashed changes
 }
-//'A','2','3','4','5','6','7','8','9','','J','Q','K'};
-int* createDeck() {
 
-static int d[52];
-int type[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-  for(int i = 0; i < 4; i++ ) {
-    for (int j = 0; j < 13; j++) {
-      d[j + i*13] = type[j % 52];
+void testHands(){
+    printf("----ALL HANDS----\n");
+    for (int i = 0; i < 4; i++){
+        printf("*Player %d hand\n",i+1);
+        for (int j = 0; j < 13; j++){
+            if(hands[i][j] != 0) {
+                printf("You have %d, %d's\n", hands[i][j], j);
+            }
+        }
     }
-  }
-  return d;
+}
+
+
+//Generates array of integers, e.g. deck of cards, there cards are integer from 0 to 12, and there are 4 of each integer.
+void createDeck() {
+    int type[13] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    for(int i = 0; i < 4; i++ ) {
+        for (int j = 0; j < 13; j++) {
+            deck[j + i*13] = type[j % 52];
+        }
+    }
 }
 
 //random from https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
-int * shuffleDeck(int* deck, int shuffleNumber) {
+void shuffleDeck(int shuffleNumber) {
     for (int i = 52-1; i > 0; i--){
         int j = shuffleNumber % (i+1);
+//        int j = (rand() % ((shuffleNumber+5) - shuffleNumber + 1)) + shuffleNumber;
         swap(&deck[i], &deck[j]);
     }
-//     //Prints deck, for DEBUG Erase in FINAL
-//     for(int i = 0; i < 52; i++) {
-//         printf("*(deck + [%d]) : %d\n", i, *(deck + i) );
-//     }
-//     return deck;
-// }
+    for(int i = 0; i < 52; i++) {
+        printf("*(deck + [%d]) : %d\n", i, *(deck + i) );
+    }
+}
 
 
-
-void swap (int *a, int*b){
+//swaps two integers by address.
+void swap (int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-  
 
-void dealCards (int hands[4][13], int numberOfPlayers, int cardPerHand, int* deck, int *deckTop) {
-   for(int i = 0; i < cardPerHand; i++){
+//Distributes integers, e.g. cards to each player from deck (numberOfPlayers - how many players there are)
+void dealCards (int numberOfPlayers, int cardPerHand) {
+    for(int i = 0; i < cardPerHand; i++){
         for(int j = 0; j < numberOfPlayers; j++){
             int card = draw(deck, deckTop);
             hands[j][card]++;
-    
         }
     }
-
-// //Prints players hands. FOR DEBUG, Erase in FINAL
-//     for(int i = 0; i < numberOfPlayers; i++){
-//         printf("*Player %d hand\n",i+1);
-//         for(int j = 0; j < 13; j++){
-//             if(hands[i][j] != 0) {
-//                 printf("You have %d, %d's\n", hands[i][j], j);
-//             }
-//         }
-//     }
-
 }
 
-
-void printOptions( int hands[4][13], int player)
-{
-  
-  printf("*Player %d hand\n",player);
-  for (int j = 0; j < 13; j++){
-      if(hands[player-1][j] != 0) {
-          printf("You have %d, %d's\n", hands[player-1][j], j);
-      }
-            
+//Prints options that player can choose. Like choose a player to take a card from.
+void printOptions(int player){
+    printf("*Player %d hand\n",player+1);
+    int hasCardInHand = 0;
+    for (int j = 0; j < 13; j++){
+        if(hands[player][j] != 0 && hands[player][j] != pairLimit) { // change hard coded 4
+            printf("You have %d, %d's\n", hands[player][j], j);
+            hasCardInHand = 1;
+        }
     }
-  printf("Which Player would you like to ask for which card in your hand? (Player Number, Card Number): \n");
-
-
+    if (!hasCardInHand){
+        printf("You can only fish, your hand is empty.\n");
+    }
+    printf("Which Player would you like to ask for which card in your hand? (Player Number, Card Number): \n");
 }
 
-<<<<<<< Updated upstream
-int draw (int*deck, int* deckTop) {
-   int card = deck[*deckTop];
-   *deckTop = *deckTop + 1;
-   return card;
+//Calls draw() to take card from deck and adds it to player's hand
+int goFish (int player, int expCard){
+    int card = draw(deck, deckTop);
+    hands[player][card]++;
+    return expCard == card;
+}
 
-=======
+//Decrease cards from one hand to increases to current player
+void moveCards(int srcPlayer, int targetPlayer, int card){
+    while(cardInHand(srcPlayer, card)) {
+        hands[srcPlayer][card]--;
+        hands[targetPlayer][card]++;
+    }
+}
+
+//Checks if targetPlayer has desired card
+int cardInHand(int targetPlayer, int card){
+    if(hands[targetPlayer][card] > 0){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//Handles the turn in Go-Fish game. It calls printOptions(), takes player input, checks is card player inputted is in the opponents hand,
+//if so calls moveCards(), otherwise calls goFish(). If player fished successfully makes recursive call to turn().
+void turn(int targetPlayer){
+    printOptions(targetPlayer);
+    int playerToAsk;
+    int cardToAsk;
+    scanf("%d", &playerToAsk);
+    scanf("%d", &cardToAsk);
+    if (cardInHand(playerToAsk, cardToAsk)){
+        moveCards(playerToAsk, targetPlayer, cardToAsk);
+        turn(targetPlayer);
+    } else {
+        if (goFish(targetPlayer, cardToAsk)){
+            turn(targetPlayer);
+        } else {
+            return;
+        }
+    }
+}
+
 //Increments Deck top and returns card
 int draw () {
     int card = deck[deckTop];
@@ -209,17 +215,14 @@ int draw () {
     return card;
 }
 
-int endGame() {
+void endGame() {
 
     printScores();
     int answer = 0;
     printf("Would you like to play again?(1 for yes, 0 for no): \n");
     scanf("%d", &answer);
     if (answer == 1) {
-        return 0;
-    }
-    else {
-        return 1;
+        startGame();
     }
 }
 
@@ -251,5 +254,4 @@ void printScores(){
             }
         }
     }
->>>>>>> Stashed changes
 }
